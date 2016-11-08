c5a5be72fcd28609a1b796980d638a2a66ec6578
chenjun <chenjun@lbesec.com>
2016-11-08 20:48:02 +0800

    添加一个toast

--- app/src/main/java/com/example/lbechenjun/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lbechenjun/myapplication/MainActivity.java
@@ -17,6 +17,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -51,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         ObjectAnimator animIn = ObjectAnimator.ofFloat(null, View.ALPHA, 0f, 1f).setDuration(2000);
         layoutTransition.setAnimator(LayoutTransition.CHANGE_DISAPPEARING, animIn);
         layoutTransition.setAnimateParentHierarchy(true);
+        Toast.makeText(this,"this is a toast",Toast.LENGTH_SHORT).show();
         revellayout.setLayoutTransition(layoutTransition);
     }
 
