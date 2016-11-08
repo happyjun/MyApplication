4995643c286af550164ef00f2bcdd71cdce4fb4c
chenjun <chenjun@lbesec.com>
2016-11-08 20:08:06 +0800

    添加项目代码

---
+++ app/src/main/java/com/example/lbechenjun/myapplication/Fragment1.java
@@ -0,0 +1,20 @@
+package com.example.lbechenjun.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by lbechenjun on 16-11-3.
+ */
+
+public class Fragment1 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment1,null,false);
+    }
+}
---
+++ app/src/main/java/com/example/lbechenjun/myapplication/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.lbechenjun.myapplication;
+
+import android.animation.LayoutTransition;
+import android.animation.ObjectAnimator;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.TransitionDrawable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FrameLayout revellayout;
+    private ImageView image2;
+    private FrameLayout container;
+    private Button  action;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        revellayout = (FrameLayout) findViewById(R.id.revel_layout);
+        image2 = (ImageView) findViewById(R.id.image_2);
+        container = (FrameLayout) findViewById(R.id.fmt_container);
+        action = (Button) findViewById(R.id.action);
+        action.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fmt_container, new Fragment1(), null).commitNowAllowingStateLoss();
+                revellayout.removeView(image2);
+            }
+        });
+        LayoutTransition layoutTransition = revellayout.getLayoutTransition();
+        if (layoutTransition == null) {
+            layoutTransition = new LayoutTransition();
+        }
+        ObjectAnimator animOut = ObjectAnimator.ofFloat(null, View.ALPHA, 1f, 0f).setDuration(2000);
+        layoutTransition.setAnimator(LayoutTransition.DISAPPEARING, animOut);
+        ObjectAnimator animIn = ObjectAnimator.ofFloat(null, View.ALPHA, 0f, 1f).setDuration(2000);
+        layoutTransition.setAnimator(LayoutTransition.CHANGE_DISAPPEARING, animIn);
+        layoutTransition.setAnimateParentHierarchy(true);
+        revellayout.setLayoutTransition(layoutTransition);
+    }
+
+
+    private void fadeInDisplay(ImageView imageView, Bitmap bitmap) {
+        final TransitionDrawable td =
+                new TransitionDrawable(new Drawable[]{
+                        new ColorDrawable(getResources().getColor(R.color.test_color)),
+                        new BitmapDrawable(imageView.getResources(), bitmap)
+                });
+        imageView.setImageDrawable(td);
+        td.startTransition(6000);
+    }
+}
