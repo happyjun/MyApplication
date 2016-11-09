95d7d691bc5d40e7d7ebb7d2ebf629ac6a69b02c
chenjun <chenjun@lbesec.com>
2016-11-09 15:36:51 +0800

    添加master toast2

--- app/src/main/java/com/example/lbechenjun/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lbechenjun/myapplication/MainActivity.java
@@ -61,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         Toast.makeText(this,"on Start()",Toast.LENGTH_SHORT).show();
         Toast.makeText(this,"on Start2()",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"on master Start2()",Toast.LENGTH_SHORT).show();
     }
 
     @Override
